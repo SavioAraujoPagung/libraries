7bf5fca101a632e1da106912581c2e2994645d4f
savioaraujopagung <savio.pagung@hotmail.com>
2024-11-13 22:18:48 -0300

    circuito

--- circuits/configuracao.cpp
+++ circuits/configuracao.cpp
@@ -1,27 +1,31 @@
 #include "configuracao.h"
 
-Configuracao::Configuracao(int id, float p, float d, float i, int16_t velocidade, uint32_t tempo, int32_t erroAcumulado) 
-{
+Configuracao::Configuracao(int id, float p, float d, float i, uint32_t velocidade, uint32_t tempo, uint32_t erroAcumulado) {
   this->id = id;
   this->p = p;
-  this->d = d;
   this->i = i;
+  this->d = d;
   this->velocidade = velocidade;
   this->tempo = tempo;
   this->erroAcumulado = erroAcumulado;
 }
 
-void Configuracao::insereValorCalibracao (char* chave, char* valor){
-    uint8_t temp8;
+void Configuracao::definirPorChave (char* chave, char* valor){
+    uint8_t tempUint8;
     float tempFloat;
+    if(strncmp(chave,"id",2)==0){
+      tempUint8 = atoi(valor);
+      this->id = tempUint8;
+      return;
+    }
     if(strncmp(chave,"setor",5)==0){
-      temp8 = atoi(valor);
-      this->id = temp8;
+      tempUint8 = atoi(valor);
+      this->id = tempUint8;
       return;
     }
     if(strncmp(chave,"velocide",8)==0){
-      temp8 = atoi(valor);
-      this->velocidade = temp8;
+      tempUint8 = atoi(valor);
+      this->velocidade = tempUint8;
       return;
     }
     if(strncmp(chave,"kp",2)==0){
@@ -41,36 +45,50 @@ void Configuracao::insereValorCalibracao (char* chave, char* valor){
     }
 }
 
-/*essa função aqui faz leitura dos dados por JSON via string, fiz meu próprio parser*/
-static bool Configuracao::calibraVeiculo(Configuracao setores[]){
-  char linhaTemp[100];
-  char temp[30];
-  char atributo[20];
-  char valor[10];
-  uint8_t quantidadeAtributos;
-  //apago coisas desnecessárias
-  //Serializacao::obtemPosicaoVetorString("leituraSerial","leituraSerial",'[',1);
-  //Serializacao::obtemPosicaoVetorString("leituraSerial","leituraSerial",']',0);
-  // int quantidadeIndices = Serializacao::retornaTamanhoVetorString("leituraSerial",'}');
+float Configuracao::obterP(){
+  return this->p;
+}
 
-  // if(quantidadeIndices == 0) return false; //algum erro ocorreu
-  
-  // for(uint8_t i=0; i < quantidadeIndices-1; i++){// 
-  //   Serializacao::obtemSubstringVetorString(linhaTemp,"leituraSerial",'{','}',i+1);
-  //   Serializacao::removeCaractere(linhaTemp, '"');
+void Configuracao::definirP(float p){
+  this->p = p;
+}
 
-  //   quantidadeAtributos = Serializacao::retornaTamanhoVetorString(linhaTemp,',');
-  //   if(quantidadeAtributos == 0) return false; //algum erro ocorreu
+float Configuracao::obterI(){
+  return this->i;
+}
 
-  //   for(uint8_t k=0; k < quantidadeAtributos; k++){
-  //     Serializacao::obtemPosicaoVetorString(temp,linhaTemp,',',k);
-  //     Serializacao::obtemPosicaoVetorString(atributo,temp,':',0);
-  //     Serializacao::obtemPosicaoVetorString(valor,temp,':',1);
-  //     setores[i].insereValorCalibracao(atributo, valor);
-  //   }
+void Configuracao::definirI(float i){
+  this->i = i;
+}
 
-  //   linhaTemp[0]=0;
-  // }
+float Configuracao::obterD(){
+  return this->d;  
+}
 
-  return true;
+void Configuracao::definirD(float d){
+  this->d = d;
+}
+
+uint32_t Configuracao::obterTempo(){
+  return this->tempo;
+}
+
+void Configuracao::definirTempo(uint32_t t){
+  this->tempo = t;
+}
+
+uint32_t Configuracao::obterVelocidade(){
+  return this->velocidade;
+}
+
+void Configuracao::definirVelocidade(uint32_t v){
+  this->velocidade = v;
+}
+
+uint32_t Configuracao::obterErroAcumulado(){
+  return this->erroAcumulado;
+}
+
+void Configuracao::definirErroAcumulado(uint32_t ea){
+  this->erroAcumulado = ea;
 }
--- circuits/configuracao.h
+++ circuits/configuracao.h
@@ -6,21 +6,39 @@
 #define QUANTIDADE_SETORES 15
 
 class Configuracao {
-  public:
-    Configuracao(int id, float p, float d, float i, int16_t velocidade, uint32_t tempo, int32_t erroAcumulado);
+  private:
     int id;
     float p;
     float d;
     float i;
-    int16_t velocidade;
+    uint32_t velocidade;
+    uint32_t tempo;
+    uint32_t erroAcumulado;
   
-    //parte do relatorio para envio
-    uint32_t tempo; //todo, savio isso aqui é a parte que enviar para o servidor o relatório feita com os dados da corrido 
-    int32_t erroAcumulado;
+  public:
+    Configuracao(int id, float p, float d, float i, uint32_t velocidade, uint32_t tempo, uint32_t erroAcumulado);
 
-    void insereValorCalibracao (char* chave, char* valor);
+    void definirPorChave (char* chave, char* valor);
+    
+    float obterP();
+    void definirP(float p);
+
+    float obterI();
+    void definirI(float i);
+    
+    float obterD();
+    void definirD(float d);
+
+    uint32_t obterTempo();
+    void definirTempo(uint32_t t);
+
+    uint32_t obterVelocidade();
+    void definirVelocidade(uint32_t v);
+
+    uint32_t obterErroAcumulado();
+    void definirErroAcumulado(uint32_t ea);
 
   /*essa função aqui faz leitura dos dados por JSON via string, fiz meu próprio parser*/
   public: 
-    static bool calibraVeiculo(Configuracao setores[]);
+    static bool calibraRobo(Configuracao setores[]);
 };
