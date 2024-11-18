a90a7cc642c92a64fbfd686cea3655e37c9706d2
savioaraujopagung <savio.pagung@hotmail.com>
2024-11-18 20:52:45 -0300

    name

--- EEPROMDatabase/EEPROMDatabase.ino
+++ EEPROMDatabase/EEPROMDatabase.ino
@@ -11,10 +11,4 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  char pista = '1';
-
-  char pistas = '12334';
-
-  Database::gravaConfiguracaoSetores(&pistas);
 }
