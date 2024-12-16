afbd83a4da7be0106ae99c35313ce54cebf1c2bf
savioaraujopagung <savio.pagung@hotmail.com>
2024-12-15 21:43:53 -0300

    exemplo carro

--- carro/carro.ino
+++ carro/carro.ino
@@ -1,13 +1,31 @@
-/*
-  Autor: Savio Araujo Pagung
-*/
-
 #include "sensor.h"
 #include "motor.h"
 
 
 void setup() {
-  // put your setup code here, to run once:
+  uint8_t pino1 = 9;
+  uint8_t pino2 = 8;
+  uint8_t pwm = 87;
+  Motor motor1 = Motor(pino1, pino2, pwm);
+
+  motor1.pino1();
+
+  int16_t velocidade = 39;
+  motor1.potencia(velocidade);
+
+
+  Sensor sensorLinha = Sensor();
+  sensorLinha.atualizaSensor();
+  sensorLinha.iniciaSensor();
+  uint8_t i = 1;
+  sensorLinha.obtemIntensidade(i);
+  
+  uint8_t s = 1;
+  sensorLinha.naLinha(s);
+  
+  uint8_t s2 = 2;
+  sensorLinha.naLinha(s, s2);
+  
 }
 
 void loop() {
